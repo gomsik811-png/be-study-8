5dcea5db506aac4a44b906f6f402c110ae0369ff
gomsik811 <gomsik811@gmail.com>
2026-05-06 13:57:10 +0900

    자바 시작, .gitignore 추가

---
+++ src/be_study/start/Helloworld.java
@@ -0,0 +1,31 @@
+package be_study.start;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hellow, world");
+		
+		
+		// 자바 코드 실행 : ctrl + f11
+		
+		//주석 메모역할
+
+		//한줄 주석
+		/*여러줄 주석
+		 * 엔터 치면서
+		 * 다음줄로내려가도
+		 * 주석으로 인식 */
+		
+//		주석 : ctrl + /
+//			  ctrl + shift + c
+		
+		//sysout 입력 -> ctrl + space 자동완성
+		
+//		System.out.println();
+		System.out.println();
+		
+	}
+
+}
