5c277b5fce8b917c19a99cfa840f4ae9a1847e1c
gomsik811 <gomsik811@gmail.com>
2026-05-07 14:25:41 +0900

    퀴즈 2 변수 기본/타입

---
+++ src/be_study/quiz/Quiz02.java
@@ -0,0 +1,40 @@
+package be_study.quiz;
+
+public class Quiz02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String brand = "브랜드명 = 로지텍";
+		String item = "mk keys";
+		int price = 135000;
+		String srl =" 332L1545K";
+		
+		//System.out.println(brand \n + item \n + "상품가격" + price \n + srl);
+
+		String company = "SAMSUNG";
+		int inch = 24;
+		String color = "black";
+		boolean fort = true;
+		boolean sound = false;
+		
+		String surccle = "3.14";
+		System.out.println(" "+surccle);
+		
+		String mathScore = "85";
+		String englishScore = "90";
+		String artScore = "70";
+		
+		System.out.println(Integer.parseInt(mathScore)+Integer.parseInt(englishScore)+Integer.parseInt(artScore));
+		
+		int num1 = 4;
+		int num2 = 10;
+		double num3 = (double)num1 / num2;
+		System.out.println(num3);
+		
+		
+		
+		
+	}
+
+}
