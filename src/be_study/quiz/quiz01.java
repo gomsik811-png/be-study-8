c5bee55d2a190097e3c6473495ae157edd9367c8
gomsik811 <gomsik811@gmail.com>
2026-05-07 12:09:55 +0900

    퀴즈 1 변수 기초

---
+++ src/be_study/quiz/quiz01.java
@@ -0,0 +1,34 @@
+package be_study.quiz;
+
+public class quiz01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int month;
+		
+		int day =29;
+		
+		month = 8;
+		
+		System.out.println("오늘은\t"+month+"월" +day+"일\t입니다.");
+		
+		String name;
+		
+		name = "류현우";
+		
+		System.out.println(name);
+		
+		String number = "010-1010-0101";
+		
+		System.out.println("전화번호는\t"+number+"\t입니다.");
+		
+		
+		
+	
+		
+		
+		
+
+	}
+
+}
