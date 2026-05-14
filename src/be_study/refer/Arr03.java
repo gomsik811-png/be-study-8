5db8536eb7819d0134c34711423790071ea99fc5
gomsik811 <gomsik811@gmail.com>
2026-05-14 11:45:15 +0900

    배열 개념

---
+++ src/be_study/refer/Arr03.java
@@ -0,0 +1,22 @@
+package be_study.refer;
+
+public class Arr03 {
+
+	public static void main(String[] args) {
+
+
+		int[] arr1 = {1,2,3};
+		
+		//           ㅁ ㅁ ㅁ
+		//           1 2 3
+		//  index	 0 1 2
+		
+		// arr[0] <-- int 타입변수
+		
+		System.out.println(arr1[0] == arr1[0]);
+		System.out.println(arr1[2] == arr1[2]);
+		
+
+	}
+
+}
