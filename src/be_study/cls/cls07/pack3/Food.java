5f3073fe23d87ace85e067051ad103db8184814c
gomsik811 <gomsik811@gmail.com>
2026-05-22 16:26:47 +0900

    디자인 패턴 - 싱글톤 패턴

--- src/be_study/cls/cls07/pack3/Food.java
+++ src/be_study/cls/cls07/pack3/Food.java
@@ -1,5 +1,54 @@
 package be_study.cls.cls07.pack3;
 
 public class Food {
+	public int id;
+	public String name;
+	public String content;
+	public int price;
+	public String description;
+	
+	//food.id
+	//food.name
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getContent() {
+		return this.content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	public int getPrice() {
+		return this.price;
+	}
+	
+	public void setPrice(String  description) {
+		this.price = price;
+	}
+	
+	public String description() {
+		return this.description;
+	}
+	
+	public void  description() {
+		this. description =  description;
+	}
+	
 
 }
--- src/be_study/cls/cls07/pack3/Point.java
+++ src/be_study/cls/cls07/pack3/Point.java
@@ -1,5 +1,80 @@
 package be_study.cls.cls07.pack3;
 
 public class Point {
+	
+	//접근제한자 (public private default)
+	//getter setter
+	
+	//점수관리
+	int math;	//수학 default
+	private int lang;	//국어 비밀 나만 접근
+	public int eng;	//영어	접근 공개
+	
+	
+	//point(){} //default
+	//private point() {}
+	public Point(){}
+	
+	
+	public Point(int mayth, int eng, int lang) {
+		this.math = math;
+		this.eng = eng;
+		this.lang = lang;
+	}
+	
+	void method1() {
+		System.out.println("method1");
+	}
+	
+	public void method2() {
+		System.out.println("method2");
+	}
+	
+	private void method3() {
+		System.out.println("method3");
+	}
+	
+	
+	//Getter Setter
+	//Getter : get 가져오기 값을 읽어오겠다.	return 값
+	//Setter : set 세팅한다 값을 저장하겠다.	변수 = 값
+	
+	//getXXX
+	//setXXX
+	
+	public int getMath(){
+		return math;
+	}
+	
+	public int getEng(){
+		return this.eng;
+	}
+	
+	public int getLang(){
+		return this.lang * 10;
+	}
+	
+	public void setMath(int math) {
+		this.math = math;
+	}
+	
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+	
+	public void setLang(int lang) {
+		//로직 추가
+		
+		//0~100점 정상범위 -> 그대로 저장
+		//범위 벗어나면 실수 -> 0점
+		if(eng >=0 && eng <= 100) {
+		this.lang = lang;
+		}else {
+			this.eng = 0;
+		}
+	}
+	
+	
+	
 
 }
