5f5a550ba5da35f7dc8469c6414750e4a02b4a7c
gomsik811 <gomsik811@gmail.com>
2026-05-26 10:26:54 +0900

    퀴즈 27 클래스 메소드

--- src/be_study/cls/cls08/SingletonMain.java
+++ src/be_study/cls/cls08/SingletonMain.java
@@ -32,6 +32,10 @@ public class SingletonMain {
 		System.out.println(s2);
 		System.out.println(s3);
 		
+		System.out.println(s1 == s2);
+		System.out.println(s1 == s3);
+		System.out.println(s2 == s3);
+		
 		
 
 	}
